f58e44ee990d4d3f772bc598b157e7b06f35a27c
zoyul <p.zoyul@gmail.com>
2024-05-20 00:11:26 +0900

    add strategy pattern example

---
+++ design-pattern/src/main/java/org/example/strategy/example/sortMain.java
@@ -0,0 +1,17 @@
+package org.example.strategy.example;
+
+import java.util.Arrays;
+import java.util.Comparator;
+
+public class sortMain {
+
+    public static void main(String[] args) {
+        Integer[] array = {1, 2, 3};
+        Arrays.sort(array);
+
+        Arrays.sort(array, Comparator.reverseOrder());
+        System.out.println(Arrays.toString(array));
+        Arrays.sort(array, Comparator.naturalOrder());
+        System.out.println(Arrays.toString(array));
+    }
+}
