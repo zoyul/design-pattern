9e4b940aa76afe2899668cf515a4c900903de814
zoyul <p.zoyul@gmail.com>
2024-05-28 01:09:45 +0900

    mod class name

--- design-pattern/src/main/java/org/example/strategy/duck3/duck3Main.java
+++ design-pattern/src/main/java/org/example/strategy/duck3/duck3Main.java
@@ -1,6 +1,6 @@
 package org.example.strategy.duck3;
 
-public class duck3Main {
+public class Duck3Main {
 
     public static void main(String[] args) {
         Duck mallardDuck = new MallardDuck();
